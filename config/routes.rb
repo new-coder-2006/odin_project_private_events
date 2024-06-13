5a1d4e154a0f71c63b3f83c95fe9e38747ce6a16
Zach Lloyd <zachlloyd@Zachs-MBP.attlocal.net>
2024-06-13 10:25:28 -0400

    added devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :events 
 
@@ -8,5 +9,5 @@ Rails.application.routes.draw do
 
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "events#index"
 end
