a4b86b7f0b00c5ad26280af7ac9d68690fa01e36
Zach Lloyd <zachlloyd@Zachs-MBP.attlocal.net>
2024-06-14 11:46:36 -0400

    split up user events into past, upcoming, and undated events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
 
     def show
         @user_events = current_user.hosted_events
-        @events_attending = current_user.attended_events
+        @current_date = Date.today
+        @future_events = current_user.attended_events.where('event_date >= ?', @current_date)
+        @past_events = current_user.attended_events.where('event_date < ?', @current_date)
+        @undated_events = current_user.attended_events.where(event_date: nil)
     end
 end
