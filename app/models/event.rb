968d40c90256a457f13533a8cc010ceac413f794
Zach Lloyd <zachlloyd@Zachs-MBP.attlocal.net>
2024-06-14 15:22:03 -0400

    refactored event model to use scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,15 +3,7 @@ class Event < ApplicationRecord
     has_many :attendances, dependent: :destroy
     has_many :attendees, through: :attendances, source: :user
 
-    def self.past
-        where("event_date < ?", Date.today)
-    end 
-
-    def self.upcoming
-        where("event_date >= ?", Date.today)
-    end 
-
-    def self.undated
-        where(event_date: nil)
-    end
+    scope :past, -> { where("event_date < ?", Date.today) }
+    scope :upcoming, -> { where("event_date >= ?", Date.today) }
+    scope :undated, -> { where(event_date: nil) }
 end
