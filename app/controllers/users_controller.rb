a85e5dc68be8b2a2fe09e7db3d6612f63ecca28d
Zach Lloyd <zachlloyd@Zachs-MBP.attlocal.net>
2024-06-14 11:11:17 -0400

    added list of attended events to user/show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
 
     def show
         @user_events = current_user.hosted_events
+        @events_attending = current_user.attended_events
     end
 end
